a9c4527c39cb97164139d1366b17c9c5f3566c0b
Yazdani <yaz4dani@gmail.com>
2022-04-22 23:54:19 +0300

    Custom Button Design

--- client/src/Components/ButtonUI.js
+++ client/src/Components/ButtonUI.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import "./buttonui.css";
 import { Paper } from "@mui/material";
-
+import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
+import { height, padding } from "@mui/system";
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  title: {
+    fontSize: 20,
+    backgroundColor: "red",
+    padding: "20px",
+    textAlign: "center",
+    color: "white",
+  },
+  btn: {
+    backgroundColor: "red",
+    color: "white",
+    borderRadius: "60px",
+  },
+});
 
 const ButtonUI = () => {
+  const classes = useStyles();
+
   const handleClick = () => {
     alert("Click is working");
   };
@@ -18,21 +36,36 @@ const ButtonUI = () => {
       <div className="button-design">
         <Stack spacing={2} direction="row">
           <Button variant="text">Text</Button>
+
           <Button
             onClick={handleClick}
             variant="contained"
-            color="success"
             size="small"
-            endIcon={<KeyboardArrowRightIcon/>}
+            style={{
+              borderRadius: 35,
+              backgroundColor: "#21b6ae",
+              padding: "18px 36px",
+              fontSize: "18px",
+            }}
+            endIcon={<KeyboardArrowRightIcon />}
           >
             Contained
           </Button>
-          <Button variant="outlined">Outlined</Button>
+
+          <Button
+            style={{
+              borderRadius: 35,
+              backgroundColor: "#21b6ae",
+              padding: "18px 36px",
+              fontSize: "18px",
+            }}
+            variant="outlined"
+          >
+            Outlined
+          </Button>
         </Stack>
-        <Paper spacing={2} >
-          <Typography>
-            New Post
-          </Typography>
+        <Paper spacing={2} sx={{ height: "350px", padding: "20px" }}>
+          <Typography className={classes.title}>New Post</Typography>
         </Paper>
       </div>
     </React.Fragment>
